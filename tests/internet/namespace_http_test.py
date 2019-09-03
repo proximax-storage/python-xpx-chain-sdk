b231f8cdcb0c8dcf361931d63937883ab32e997f
wirfeon <wirfeon@gmail.com>
2019-09-03 10:03:12 +0200

    Namespace test fixed

--- tests/internet/namespace_http_test.py
+++ tests/internet/namespace_http_test.py
@@ -8,37 +8,37 @@ from tests import harness
     'tests': [
         {
             'name': 'test_get_namespaces_from_account',
-            'params': [models.Address('SD3MA6SM7GWRX4DEJVAZEGFXF7G7D36MA6TMSIBM')],
+            'params': [models.Address('SARNASAS2BIAB6LMFA3FPMGBPGIJGK6IJETM3ZSP')],
             'method': 'get_namespaces_from_account',
             'validation': [
-                lambda x: (len(x), 1),
-                lambda x: (x[0].meta_id, '5C7C07005CC1FE000176FA2B'),
+                lambda x: (len(x), 3),
+                lambda x: (x[0].meta_id, '5D62745F8E825C00011B7CB6'),
             ]
         },
         {
             'name': 'test_get_namespaces_from_accounts',
-            'params': [[models.Address('SD3MA6SM7GWRX4DEJVAZEGFXF7G7D36MA6TMSIBM')]],
+            'params': [[models.Address('SARNASAS2BIAB6LMFA3FPMGBPGIJGK6IJETM3ZSP')]],
             'method': 'get_namespaces_from_accounts',
             'validation': [
-                lambda x: (len(x), 1),
-                lambda x: (x[0].meta_id, '5C7C07005CC1FE000176FA2B'),
+                lambda x: (len(x), 3),
+                lambda x: (x[0].meta_id, '5D62745F8E825C00011B7CB6'),
             ]
         },
         {
             'name': 'test_get_namespaces_name',
-            'params': [[models.NamespaceId.create_from_hex('84b3552d375ffa4b')]],
+            'params': [[models.NamespaceId.create_from_hex('b16d77fd8b6fb3be')]],
             'method': 'get_namespaces_name',
             'validation': [
                 lambda x: (len(x), 1),
-                lambda x: (x[0].name, 'nem'),
+                lambda x: (x[0].name, 'prx'),
             ]
         },
         {
             'name': 'test_get_namespace',
-            'params': [models.NamespaceId.create_from_hex('84b3552d375ffa4b')],
+            'params': [models.NamespaceId.create_from_hex('b16d77fd8b6fb3be')],
             'method': 'get_namespace',
             'validation': [
-                lambda x: (x.meta_id, '5C7C07005CC1FE000176FA2B'),
+                lambda x: (x.meta_id, '5D62745F8E825C00011B7CB5'),
             ]
         },
     ],
