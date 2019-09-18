09d33f073ae25efa2d16642fc6c352dd10a4574b
wirfeon <wirfeon@gmail.com>
2019-09-18 07:56:47 +0200

    Tests for /transaction​/{hash}​/status and /transaction​/statuses

--- tests/internet/transaction_http_test.py
+++ tests/internet/transaction_http_test.py
@@ -25,6 +25,25 @@ from tests import config
                 lambda x: (isinstance(x[0], models.Transaction), True),
             ]
         },
+        {
+            #/transaction/{hash}/status
+            'name': 'test_get_transaction_status',
+            'params': [config.Transaction.hash],
+            'method': 'get_transaction_status',
+            'validation': [
+                lambda x: (isinstance(x, models.TransactionStatus), True),
+            ]
+        },
+        {
+            #/transaction/statuses
+            'name': 'test_get_transaction_statuses',
+            'params': [[config.Transaction.hash, '024C08B8767FCA0DCF7B631EC2631D9575FFB84E8E5EFA7C656B18FB1A1F34E8']],
+            'method': 'get_transaction_statuses',
+            'validation': [
+                lambda x: (len(x), 2),
+                lambda x: (isinstance(x[0], models.TransactionStatus), True),
+            ]
+        },
     ],
 })
 class TestTransactionHttp(harness.TestCase):
